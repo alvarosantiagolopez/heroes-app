9a881c53170ddbb55a5ecc81ab19000d4f9910dd
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-12-23 07:46:32 +0100

    login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,17 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+
+    const { login } = useContext(AuthContext)
 
     const navigate = useNavigate();
 
     const onLogin = () => {
+
+        login('Alvaro Santiago')
+
         navigate('/', {
             replace: true
         })
@@ -12,11 +19,11 @@ export const LoginPage = () => {
     }
 
     return (
-        <div>
-            <h1 className='m-3'>Login</h1>
+        <div className='container mt-5'>
+            <h1>Login</h1>
             <hr />
 
-            <button className="btn btn-primary m-3"
+            <button className="btn btn-primary"
                 onClick={onLogin}
             >
                 Login
