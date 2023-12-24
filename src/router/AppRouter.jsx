c857874881ec8fc6cad9e99fdb4b32f2e5ed4c73
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-12-24 08:40:13 +0100

    private and public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from '../heroes/routes/PrivateRoute';
+import { PublicRoute } from '../heroes/routes/PublicRoute';
 
 
 
@@ -9,8 +11,19 @@ export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPage />} />
-                <Route path="/*" element={<HeroesRoutes />} />
+
+                <Route path="login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
+
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                } />
+
             </Routes>
 
         </>
