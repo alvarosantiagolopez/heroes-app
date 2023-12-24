27ab96328d25d9a3724cd43586b461a2fb6a99f2
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-12-24 08:53:24 +0100

    remember last page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
     const onLogin = () => {
 
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
         login('Alvaro Santiago')
 
-        navigate('/', {
+        navigate(lastPath, {
             replace: true
         })
 
